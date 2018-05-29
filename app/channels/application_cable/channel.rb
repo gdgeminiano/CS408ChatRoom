8ce0868e854c589319cc2ea006f330d2566b0b66
unknown <geriannag96@gmail.com>
2018-05-28 20:37:00 -0700

    more comments

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,3 +1,5 @@
+# Channel class: for shared logic amoung channels
+# Only using 1 channel (room_channel) so we dont need to edit this
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
   end
--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,5 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
+# Connection class: authrorizes incoming connection
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_user
